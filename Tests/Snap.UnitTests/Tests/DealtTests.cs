c376231a1a90475256ed69b0aaa141a22c1c6a55
Dawlin Pena Luciano <dawmusic19@gmail.com>
2019-02-21 21:25:44 +0000

    Dealt test pass

--- Tests/Snap.UnitTests/Tests/DealtTests.cs
+++ Tests/Snap.UnitTests/Tests/DealtTests.cs
@@ -2,10 +2,12 @@
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Dawlin.Util;
 using GameSharp.Entities;
 using Microsoft.Extensions.DependencyInjection;
 using Shouldly;
 using Snap.DI;
+using Snap.Entities;
 using Snap.Entities.Enums;
 using Snap.Fakes;
 using Snap.Services.Abstract;
@@ -19,30 +21,34 @@ namespace Snap.Tests.Tests
         [Fact]
         public async Task When_dealting_then_the_cards_of_each_player_should_not_be_repeated()
         {
-            using (var module = await TestModuleHelpers
-                    .CreateAndBuildWithDefaultsAsync())
+            using (var module = await TestModuleHelpers.CreateAndBuildWithDefaultsAsync())
             {
-                var service = module.GetService<ISnapGameServices>();
-                var game = (await service.CreateAsync(CancellationToken.None));
-                (await service.StarGameAsync(game, CancellationToken.None))
-                    .PlayersData
+                //Background or When
+                var game = await module.CreateGameAsync();
+                await module.SecondPlayerJoin(game);
+                game = await module.GameStart(game);
+
+                //Then
+                game.PlayersData
                     .Select(p => p.StackEntity)
                     .ToList().ForEach(playerStack =>
-                {
-                    playerStack.ToList().Select(s => s.Card).ShouldBeUnique();
-                });
+                    {
+                        playerStack.ToList().Select(s => s.Card).ShouldBeUnique();
+                    });
             }
         }
 
         [Fact]
         public async Task When_dealting_then_none_of_the_cards_should_be_repeated()
         {
-            using (var module = await TestModuleHelpers
-                    .CreateAndBuildWithDefaultsAsync())
+            using (var module = await TestModuleHelpers.CreateAndBuildWithDefaultsAsync())
             {
-                var service = module.GetService<ISnapGameServices>();
-                var game = (await service.CreateAsync(CancellationToken.None));
-                game = await service.StarGameAsync(game, CancellationToken.None);
+                //Background or When
+                var game = await module.CreateGameAsync();
+                await module.SecondPlayerJoin(game);
+                game = await module.GameStart(game);
+
+                //Then
                 game.PlayersData
                     .SelectMany(p => p.StackEntity)
                     .Select(s => s.Card).ShouldBeUnique();
@@ -52,13 +58,15 @@ namespace Snap.Tests.Tests
         [Fact]
         public async Task When_dealting_the_cards_every_player_should_have_a_proportional_amount_of_cards()
         {
-            using (var module = await TestModuleHelpers
-                .CreateAndBuildWithDefaultsAsync())
+            using (var module = await TestModuleHelpers.CreateAndBuildWithDefaultsAsync())
             {
-                var service = module.GetService<ISnapGameServices>();
+                //Background or When
+                var game = await module.CreateGameAsync();
+                await module.SecondPlayerJoin(game);
+                game = await module.GameStart(game);
+
+                //Then
                 var fakePlayerService = module.GetService<IFakePlayerService>();
-                var game = (await service.CreateAsync(CancellationToken.None));
-                game = await service.StarGameAsync(game, CancellationToken.None);
                 var playersStacks = game.PlayersData.Select(p => p.StackEntity).ToList();
                 var cardsPerPlayer = Enum.GetValues(typeof(Card)).Length
                                      / (await fakePlayerService.GetPlayers()).Count();
