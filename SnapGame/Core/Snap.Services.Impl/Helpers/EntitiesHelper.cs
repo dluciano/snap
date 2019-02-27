b513e7d3688adac2910ef5d4742a130761489622
Dawlin Pena Luciano <dawmusic19@gmail.com>
2019-02-27 16:52:28 +0000

    EntityHelper class added

---
+++ SnapGame/Core/Snap.Services.Impl/Helpers/EntitiesHelper.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using Snap.Entities;
+
+namespace Snap.Services.Impl.Helpers
+{
+    internal static class EntitiesHelper
+    {
+        public static IIncludableQueryable<SnapGame, StackNode> IncludeAll(this DbSet<SnapGame> repo)
+        {
+            return repo
+                .Include(g => g.CentralPile.Last)
+                .ThenInclude(p => p.Previous)
+
+                .Include(g => g.GameData)
+                .ThenInclude(gd => gd.CurrentTurn)
+                .ThenInclude(pt => pt.Next)
+                .Include(p => p.GameData.CurrentTurn.Player)
+
+                .Include(p => p.PlayersData)
+                .ThenInclude(pd => pd.PlayerTurn)
+                .ThenInclude(pt => pt.Player)
+
+                .Include(p => p.PlayersData)
+                .ThenInclude(pd => pd.PlayerTurn)
+                .ThenInclude(pt => pt.Next)
+
+                .Include(p => p.PlayersData)
+                .ThenInclude(pd => pd.StackEntity.Last)
+                .ThenInclude(n => n.Previous);
+        }
+    }
+}
